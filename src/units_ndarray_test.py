02b7ffb11e00de040b2869be9166b61c64cc0096
Marija Kekic <mmkekic@gmail.com>
2019-11-25 09:45:52 +0100

    Fix import in test

--- src/units_ndarray_test.py
+++ src/units_ndarray_test.py
@@ -1,7 +1,11 @@
 import pytest
 from pytest import mark
+
 import units_ndarray as u
+import numpy         as np
+
 
 @mark.parametrize("units", ['kg', 'N', 's'])
 def test_creates_class(units):
     u.phval(1., units)
+
