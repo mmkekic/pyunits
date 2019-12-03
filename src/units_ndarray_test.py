c623cfc2ae7bc10e01620f7fc1838d38facf10a6
jsalvado <jor.salvado@gmail.com>
2019-12-03 12:45:06 +0100

    add test for operations

--- src/units_ndarray_test.py
+++ src/units_ndarray_test.py
@@ -9,3 +9,24 @@ import numpy         as np
 def test_creates_class(units):
     u.phval(1., units)
 
+
+@mark.parametrize("units", [['kg*m/s^2', 'N']])
+def test_operations(units):
+    x=u.phval(1, units[0])
+    y=u.phval(1, units[1])
+    print(x, units[0])
+    print(y, units[1])
+    z=x+y
+    assert float(z) == float(x)+float(y)
+    assert z.units == x.units == y.units
+    z=x-y
+    assert float(z) == float(x)-float(y)
+    assert z.units == x.units == y.units
+    z=x/y
+    assert float(z) == float(x)/float(y)
+    assert z.units == x.units - y.units
+    z=x*y
+    assert float(z) == float(x)*float(y)
+    assert z.units == x.units + y.units
+
+print(u.phval.strunit(1.,"N"))
